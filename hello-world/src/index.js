ce071a93b91355b14f7c4cdeacd826f34e13be8a
Yamashou <1230124fw@gmail.com>
2017-03-17 15:33:38 +0900

    動作正常稼動確認

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -1,46 +1,118 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import $ from 'jquery';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
-// class Clock extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {date: new Date()};
-  // }
-//
-//   componentDidMount() {
-//     this.timerID = setInterval(
-//       () => this.tick(),
-//       1000
-//     );
-//   }
-//
-//   componentWillUnmount() {
-//     clearInterval(this.timerID);
-//   }
-//
-//   tick() {
-//     this.setState({
-//       date: new Date()
-//     });
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//         <h1>Hello, world!</h1>
-//         <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-//       </div>
-//     );
-//   }
-// }
+// import fetch from 'isomorphic-fetch';
+export default class App extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {word: '',
+      items :[]
+    };
+  }
+  componentDidMount() {
+    this.UserList();
+    this.timerID = setInterval(
+      () => this.UserList(),
+      30000
+    );
+  }
 
-// ReactDOM.render(
-//   <Clock />,
-//   document.getElementById('root')
-// );
+  UserList() {
+  return $.ajax({
+    url: "http://localhost:3001/api/all",
+    dataType: 'jsonp',
+    type: 'GET',
+    success: function(data) {
+      this.setState({items: data});
+    }.bind(this),
+    error: function(xhr, status, err) {
+      console.error("http://localhost:3001/api/add", status, err.toString());
+    }
+  });
+}
+  handleChange(e) {
+    this.setState({word: e.target.value});
+  }
+
+
+ componentWillUnmount() {
+   clearInterval(this.timerID);
+ }
+
+
+ dropData(e){
+   const number = e.currentTarget.getAttribute('data-number');
+   console.log(number);
+   $.ajax({
+     url: "http://localhost:3001/api/delete?id=" + number,
+     dataType: 'jsonp',
+     type: 'GET',
+     success: function(data) {
+       this.UserList();
+     }.bind(this),
+     error: function(xhr, status, err) {
+       console.error("http://localhost:3001/api/delete", status, err.toString());
+     }
+   });
+}
+
+
+
+
+  changeText() {
+    console.log(this);
+    $.ajax({
+      url: "http://localhost:3001/api/add",
+      dataType: 'jsonp',
+      type: 'POST',
+      data: {name : this.state.word},
+      success: function(data) {
+        this.UserList();
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error("http://localhost:3001/api/add", status, err.toString());
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2><label class="col-sm-2 col-form-label">Word:</label></h2>
+        <input
+          onChange={this.handleChange.bind(this)} />
+        <button onClick={this.changeText.bind(this)}>change</button>
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>ワード</th>
+              <th>ツイート数</th>
+              <th>開始日時</th>
+              <th>削除</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.items.map(item => {
+              return (<tr key={item.id}>
+                <td>{ item.id }</td>
+                <td>{ item.q }</td>
+                <td>{ item.tweets_num }</td>
+                <td>{ item.create_at }</td>
+                <td><button type="button" onClick={this.dropData.bind(this)} data-number={ item.id } >Delete</button></td>
+              </tr>);
+            })}
+            <getList />
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+
+  ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
